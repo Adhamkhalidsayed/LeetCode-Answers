c1d7978b18a6f80fab39493705dd9e87bc7e980a
Adham Khalid Hammad <96948853+Adhamkhalidsayed@users.noreply.github.com>
2025-02-22 08:13:42 +0200

    Time: 2 ms (23.68%), Space: 17.7 MB (87.81%) - LeetHub

--- 1980-find-unique-binary-string/1980-find-unique-binary-string.py
+++ 1980-find-unique-binary-string/1980-find-unique-binary-string.py
@@ -1,13 +1,11 @@
 class Solution:
     def findDifferentBinaryString(self, nums: List[str]) -> str:
         nums.sort()
-        zeros = "0" * len(nums[0])
-        ones = "1" * len(nums[0])
         
-        if zeros not in nums:
-            return zeros
-        if ones not in nums:
-            return ones
+        if "0" * len(nums[0]) not in nums:
+            return "0" * len(nums[0])
+        if "1" * len(nums[0]) not in nums:
+            return "1" * len(nums[0])
         else:
             for i in range(int(nums[-1])+1):
                 n_bin= bin(i)[2:]
