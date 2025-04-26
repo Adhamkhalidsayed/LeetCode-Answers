d03f153fea1aa02c619cbd947fc850acf4604ec8
Adham Khalid Hammad <96948853+Adhamkhalidsayed@users.noreply.github.com>
2025-04-26 18:29:31 +0300

    04-26-2025 114 ms (39.47%) 0121-best-time-to-buy-and-sell-stock

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -1,13 +1,16 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        purchase_price = prices[0]
+        l = 0
+        r = 1
+        max_profit = 0
 
-        profit = 0
-
-        for sell_price in prices[1:]:
-            if purchase_price > sell_price:
-                purchase_price = sell_price
+        while r < len(prices):
+            if prices[l] > prices[r]:
+                l = r
+                r += 1
+            elif prices[l] < prices[r]:
+                max_profit = max(max_profit, prices[r] - prices[l])
+                r += 1
             else:
-                profit = max(profit, sell_price-purchase_price)
-
-        return profit
+                r += 1 
+        return(max_profit)
