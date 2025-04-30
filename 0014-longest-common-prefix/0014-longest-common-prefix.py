3771950b3849e23649cd11099643cb365895edde
Adham Khalid Hammad <96948853+Adhamkhalidsayed@users.noreply.github.com>
2025-04-30 17:35:21 +0300

    04-30-2025 0 ms (100%) 0014-longest-common-prefix

--- 0014-longest-common-prefix/0014-longest-common-prefix.py
+++ 0014-longest-common-prefix/0014-longest-common-prefix.py
@@ -1,15 +1,24 @@
 class Solution:
-    def longestCommonPrefix(self, strs: List[str]) -> str:
-        min_length = float("inf")
-        for s in strs:
-            min_length = len(s) if len(s) < min_length else min_length
+    def longestCommonPrefix(self, string: List[str]) -> str:
+        if "" in string:
+            return ""
 
-        i = 0
-        while i < (min_length):
-            for str in strs:
-                if str[i] != strs[0][i]:
-                    return str[:i]
-            i += 1
-            
-        return s[:i]
+        shortest_string = ""
+        for i in string:
+            if len(i) < len(shortest_string) or shortest_string == "":
+                shortest_string = i
+
+        j = 0
+        while j < len(string):
+            if string[j][0] == shortest_string[0]:
+                if j == shortest_string:
+                    j=j+1
+                elif shortest_string in string[j][:len(shortest_string)]:
+                    j += 1
+                elif shortest_string not in string[j][:len(shortest_string)]:
+                    shortest_string = shortest_string[:-1]
+                    j -= 1
+            else:
+                return ""
+        return shortest_string
     
