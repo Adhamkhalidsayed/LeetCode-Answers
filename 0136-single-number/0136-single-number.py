fd992ebd69ab86f5b7cdec4d1826857d85cd24ea
Adham Khalid Hammad <96948853+Adhamkhalidsayed@users.noreply.github.com>
2025-05-02 03:41:08 +0300

    05-02-2025 3 ms (65.64%) 0136-single-number

--- 0136-single-number/0136-single-number.py
+++ 0136-single-number/0136-single-number.py
@@ -1,10 +1,6 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        nums.sort()
-        i = 0
-        while i+1 < len(nums):
-            if nums[i] != nums[i+1] and nums[i] != nums[i-1]:
-                break
-            else:
-                i+=1
-        return nums[i]
+        out = 0
+        for i in nums:
+            out = out ^ i
+        return out
