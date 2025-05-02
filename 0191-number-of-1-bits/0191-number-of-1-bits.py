7be5b1680cb93d992437232bece5a3cc091a95ba
Adham Khalid Hammad <96948853+Adhamkhalidsayed@users.noreply.github.com>
2025-05-03 02:45:52 +0300

    05-03-2025 4 ms (4.23%) 0191-number-of-1-bits

--- 0191-number-of-1-bits/0191-number-of-1-bits.py
+++ 0191-number-of-1-bits/0191-number-of-1-bits.py
@@ -1,12 +1,8 @@
 class Solution:
     def hammingWeight(self, n: int) -> int:
         numOfOnes = 0
-        while n > 0:
-            x = n // 2
-            y = n / 2
-            
-            if x != y:
+        while n:
+            if n % 2 == 1:
                 numOfOnes += 1
-            n = x
-        
+            n = n // 2
         return numOfOnes
