442fa66f2e4d1a37cd4ee9275aa189d3b0f1cb8b
Adham Khalid Hammad <96948853+Adhamkhalidsayed@users.noreply.github.com>
2025-05-03 22:38:52 +0300

    05-03-2025 36 ms (76.06%) 0190-reverse-bits

---
+++ 0190-reverse-bits/0190-reverse-bits.py
@@ -0,0 +1,11 @@
+class Solution:
+    def reverseBits(self, n: int) -> int:
+        
+        x = 0
+        num = 0
+        for i in range(31,-1,-1):
+            x = n & 1
+            n = n >> 1
+            num += x * (2**i)
+        
+        return num
